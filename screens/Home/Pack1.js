d53f5949366275d6e554a1bc2febe3d140e31f24
Yash <yashchauhan335.yc@gmail.com>
2021-02-28 10:02:54 +0530

    Start Expo App

---
+++ screens/Home/Pack1.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+export function Header(){
+    return(
+        <View>
+            
+        </View>
+    );
+}
+
+const style = StyleSheet.create({
+    header:{
+        flex:1,
+        backgroundColor:'green'
+    }
+});
---
+++ screens/Home/main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native";
+// import {  } from "";
+
+export default function Home(){
+    return(
+        <View>
+            <Text>
+                1234334
+            </Text>
+        </View>
+    )
+}
